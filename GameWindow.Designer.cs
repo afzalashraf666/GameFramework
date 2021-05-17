cec222af7064c43cdbe15bd6679a0392c26cfe87
afzalashraf666 <afzalashraf814@gmail.com>
2021-05-18 01:03:22 +0500

    Added enemies moving animation

--- GameWindow.Designer.cs
+++ GameWindow.Designer.cs
@@ -29,20 +29,96 @@ namespace GameFramework
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.mainTimer = new System.Windows.Forms.Timer(this.components);
+            this.picBox3 = new System.Windows.Forms.PictureBox();
+            this.picBox4 = new System.Windows.Forms.PictureBox();
+            this.picBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // picBox
+            // 
+            this.picBox.BackColor = System.Drawing.Color.Transparent;
+            this.picBox.Image = global::GameFramework.Properties.Resources.robberrub;
+            this.picBox.Location = new System.Drawing.Point(637, 282);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(150, 150);
+            this.picBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox.TabIndex = 0;
+            this.picBox.TabStop = false;
+            // 
+            // mainTimer
+            // 
+            this.mainTimer.Enabled = true;
+            this.mainTimer.Tick += new System.EventHandler(this.mainTimer_Tick);
+            // 
+            // picBox3
+            // 
+            this.picBox3.BackColor = System.Drawing.Color.Transparent;
+            this.picBox3.Image = global::GameFramework.Properties.Resources.robberrub;
+            this.picBox3.Location = new System.Drawing.Point(353, 282);
+            this.picBox3.Name = "picBox3";
+            this.picBox3.Size = new System.Drawing.Size(150, 150);
+            this.picBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox3.TabIndex = 2;
+            this.picBox3.TabStop = false;
+            // 
+            // picBox4
+            // 
+            this.picBox4.BackColor = System.Drawing.Color.Transparent;
+            this.picBox4.Image = global::GameFramework.Properties.Resources.robberrub;
+            this.picBox4.Location = new System.Drawing.Point(219, 282);
+            this.picBox4.Name = "picBox4";
+            this.picBox4.Size = new System.Drawing.Size(150, 150);
+            this.picBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox4.TabIndex = 3;
+            this.picBox4.TabStop = false;
+            // 
+            // picBox2
+            // 
+            this.picBox2.BackColor = System.Drawing.Color.Transparent;
+            this.picBox2.Image = global::GameFramework.Properties.Resources.robberrub;
+            this.picBox2.Location = new System.Drawing.Point(492, 282);
+            this.picBox2.Name = "picBox2";
+            this.picBox2.Size = new System.Drawing.Size(150, 150);
+            this.picBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox2.TabIndex = 4;
+            this.picBox2.TabStop = false;
             // 
             // GameWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::GameFramework.Properties.Resources.GameScene1__1_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.picBox2);
+            this.Controls.Add(this.picBox4);
+            this.Controls.Add(this.picBox3);
+            this.Controls.Add(this.picBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "GameWindow";
             this.Text = "GameWindow";
+            this.Load += new System.EventHandler(this.GameWindow_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private System.Windows.Forms.Timer mainTimer;
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.PictureBox picBox3;
+        private System.Windows.Forms.PictureBox picBox4;
+        private System.Windows.Forms.PictureBox picBox2;
     }
 }
 
--- GameWindow.cs
+++ GameWindow.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Collections;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -12,9 +14,34 @@ namespace GameFramework
 {
     public partial class GameWindow : Form
     {
+        Game g = new Game();
         public GameWindow()
         {
             InitializeComponent();
+        }
+
+        private void GameWindow_Load(object sender, EventArgs e)
+        {
+            GameObject newobj = new GameObject(picBox, 1);
+            g.addGameObject(newobj);
+            GameObject newobj2 = new GameObject(picBox2, 2);
+            g.addGameObject(newobj2);
+            GameObject newobj3 = new GameObject(picBox3, 3);
+            g.addGameObject(newobj3);
+            GameObject newobj4 = new GameObject(picBox4, 3);
+            g.addGameObject(newobj4);
+        }
+
+        private void mainTimer_Tick(object sender, EventArgs e)
+        {
+            ArrayList tempList = new ArrayList();
+            tempList = g.returnObjects();
+
+            for (int idx = 0; idx < tempList.Count; idx++)
+            {
+                GameObject newObj = (GameObject)tempList[idx];
+                newObj.UpdatePosition();
+            }
         }
     }
 }
