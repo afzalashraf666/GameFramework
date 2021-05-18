d530d45e2fae3bd3afc4b65c069bb7821e15d163
afzalashraf666 <afzalashraf814@gmail.com>
2021-05-18 07:54:41 +0500

    Added enemies moving animation

---
+++ Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFramework
+{
+    class Game
+    {
+        private ArrayList objectsArrayList = new ArrayList();
+
+        public void addGameObject(GameObject newObj)
+        {
+            objectsArrayList.Add(newObj);
+        }
+
+        public ArrayList returnObjects()
+        {
+            return objectsArrayList;
+        }
+    }
+}
---
+++ GameObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameFramework
+{
+    class GameObject
+    {
+        private int speed;
+        PictureBox gameElement = new PictureBox();
+
+        public GameObject(PictureBox gameElement, int speed)
+        {
+            this.speed = speed;
+            this.gameElement = gameElement;
+
+        }
+
+        public void UpdatePosition()
+        {
+            this.gameElement.Left -= 5;
+        }
+    }
+}
